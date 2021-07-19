c825288ce1880f6a0e78790c0959526a0e80d430
Nahuel Bullón <nahuelbullonjensen@gmail.com>
2021-07-19 18:47:40 -0300

    segundo arreglo

---
+++ HolaMundoConMavenDesdeNetBeans/MavenHolaMundo/src/main/java/com/mycompany/mavenholamundo/MavenHolaMundo.java
@@ -0,0 +1,12 @@
+package com.mycompany.mavenholamundo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenHolaMundo {
+    public static void main(String[] args) {
+        SpringApplication.run(MavenHolaMundo.class, args);
+    }
+    
+}
---
+++ HolaMundoConMavenDesdeSpringInitializr/holamundonahuel/src/test/java/com/eggcursos/holamundonahuel/HolamundonahuelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eggcursos.holamundonahuel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolamundonahuelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
